22a10657834337c6759e151a34ff04b044eaa050
DKooijman97 <48057587+DKooijman97@users.noreply.github.com>
2021-08-13 09:39:34 +0200

    Changed main app to my app, changed requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from mathModel import linearFunction
+import plotly.express as px
 
-"""
-# Welcome to Dennis' first Streamlit App!
+st.write(""""
+#My First app
+*Hello *world!*
+""")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+a = st.number_input('Enter a number for the slope', value = 1)
+b = st.number_input('Enter a number for the intercept', value = 0)
+step = st.number_input('Enter a number for the step', value = 1)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+values = st.slider('Select a range of values',0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+df = linearFunction(b=b, a=a, gridInfo = [values[0], values[1] +1, step])
+fig = px.scatter(df, x="x", y="y", trendline="ols")
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.plotly_chart(fig)
